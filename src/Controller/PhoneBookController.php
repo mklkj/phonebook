546baa528070595d38e3611b014035e011e04f16
Mikołaj Pich <m.pich@outlook.com>
2022-04-27 21:37:16 +0200

    Initial commit

---
+++ src/Controller/PhoneBookController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PhoneBookController extends AbstractController
+{
+
+    #[Route('/')]
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('index.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
